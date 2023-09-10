17dacda48736c60e3fcc2d849d847587ce5eea24
= <piyeris0@gmail.com>
2023-09-10 18:30:30 -0400

    move port to env

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
+import 'dotenv/config'
 import express from 'express';
 
-const port = 3000;
 export const app = express();
 
-app.listen(port, () => { console.log(`Server running on port ${port}`); })
+app.listen(process.env.PORT, () => { console.log(`Server running on port ${process.env.PORT}`); })
