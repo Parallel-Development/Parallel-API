04a65214d03c0915dce1d76f0c37fd70edcb7bde
= <piyeris0@gmail.com>
2023-09-10 21:50:41 -0400

    expand

--- src/routes/simple.ts
+++ src/routes/simple.ts
@@ -3,6 +3,30 @@
 import { app } from '../app';
 import sql from '../db';
 
+app.get('/', async (req, res) => {
+  const options = {
+    root: 'public',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+
+  return res.sendFile('home.html', options);
+});
+
+app.get('/style', async (req, res) => {
+  const options = {
+    root: 'public',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+
+  return res.sendFile('home.css', options);
+})
+
 app.get('/chatlog/:id', async (req, res) => {
   const query = await sql`SELECT "html" FROM "Chatlog" WHERE id = ${req.params.id}`;
   if (query.length === 0) return res.status(404).send('No chat log with that ID exists. It may have been automatically deleted.');
@@ -10,8 +34,12 @@ app.get('/chatlog/:id', async (req, res) => {
 });
 
 app.get('/invite', async (req, res) => {
-  return res.redirect(`https://discord.com/api/oauth2/authorize?client_id=745401642664460319&permissions=3154439422&scope=bot%20applications.commands`);
+  return res.redirect('https://discord.com/api/oauth2/authorize?client_id=745401642664460319&permissions=3154439422&scope=bot%20applications.commands');
 });
+
+app.get('/discord', async (req, res) => {
+  return res.redirect('https://discord.gg/v2AV3XtnBM')
+})
 
 app.get('*', async (req, res) => {
   return res.status(404).send('Page does not exist.');
